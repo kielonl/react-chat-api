b58de633ce6508c376a9a970509390fa91ab3e8b
Milosz Stachlewski <milosz.stachlewski@gmail.com>
2022-07-12 14:36:57 +0200

    applied suggestions to code

--- channels.js
+++ channels.js
@@ -1,4 +1,3 @@
-const e = require("express");
 const crypto = require("crypto");
 const users = require("./users").users;
 let channels = [];
@@ -7,28 +6,26 @@ module.exports = function (app) {
   app.post("/channels", (request, response) => {
     const found = users.some((el) => el.username == request.body.username);
     if (!found) {
-      response.status(400);
-      response.send({
-        message: "There's no user with this username",
-      });
+      response
+        .status(401)
+        .json({ errorMessage: "There's no user with this username" });
     } else {
       if (
         request.body.maxUsers > 128 ||
         request.body.maxUsers < 2 ||
         !Number.isInteger(request.body.maxUsers)
       ) {
-        response.status(400);
-        response.send({
-          message: "Users amount must be a number between 2 and 128",
+        response.status(401).json({
+          errorMessage: "Users amount must be a number between 2 and 128",
         });
       } else {
-        const ChannelOwner = {
+        const channelOwner = {
           owner: request.body.username,
           channelUuid: crypto.randomUUID(),
           dataTime: new Date().toString(),
           maxNumberOfMembers: request.body.maxUsers,
         };
-        channels.push(ChannelOwner);
+        channels.push(channelOwner);
         response.status(201);
         response.send(channels);
       }
