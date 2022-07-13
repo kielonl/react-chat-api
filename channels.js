11b958a1431478ea912280c4c5742f0f7017e17f
kielon <mosadowski23@gmail.com>
2022-07-13 13:20:40 +0200

    pakiecik 2 i 3

--- channels.js
+++ channels.js
@@ -2,16 +2,26 @@ const crypto = require("crypto");
 const users = require("./users").users;
 let channels = [];
 
+const isValidUUID = (uuid) => {
+  const regexExp =
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+  return regexExp.test(uuid);
+};
+
 const usernameExists = (givenUuid) => {
   return users.some((el) => el.uuid == givenUuid);
 };
 
 module.exports = function (app) {
   app.post("/channels", (request, response) => {
-    if (!usernameExists(request.body.uuid)) {
-      return response
-        .status(400)
-        .json({ errorMessage: "There's no user with this UUID" });
+    if (!isValidUUID(request.body.uuid)) {
+      return response.status(400).json({ errorMessage: "UUID is invalid" });
+    } else {
+      if (!usernameExists(request.body.uuid)) {
+        return response
+          .status(400)
+          .json({ errorMessage: "There's no user with this UUID" });
+      }
     }
     if (
       request.body.maxUsers > 128 ||
@@ -36,11 +46,17 @@ module.exports = function (app) {
     response.send(channels);
   });
   app.get("/channels/:uuid", (request, response) => {
-    const result = channels.find((x) => x.channelUuid == request.params.uuid);
-    if (result) {
-      response.status(200).json(result);
+    if (isValidUUID(request.params.uuid)) {
+      const result = channels.find((x) => x.channelUuid == request.params.uuid);
+      if (result) {
+        response.status(200).json(result);
+      } else {
+        response.status(400).json({ errorMessage: "Channel UUID not found" });
+      }
     } else {
-      response.status(400).json({ errorMessage: "Channel UUID not found" });
+      response
+        .status(400)
+        .json({ errorMessage: "given UUID format is invalid" });
     }
   });
 };
--- users.js
+++ users.js
@@ -3,10 +3,10 @@ const axios = require("axios");
 const crypto = require("crypto");
 const users = [];
 
-const hasWhiteSpaces = (string) => {
-  return /\s/.test(string);
+const hasWhiteSpaces = (username) => {
+  return /\s/.test(username);
 };
-const isLenghtOk = (string) => {
+const isLengthOk = (string) => {
   return string.length >= 22 || string.length < 5;
 };
 const isNotANumber = (string) => {
@@ -14,7 +14,7 @@ const isNotANumber = (string) => {
 };
 const isImage = (url) => {
   if (url.length < 2048) {
-    return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+    return /\.(jpg|jpeg|png)$/.test(url);
   } else {
     return false;
   }
@@ -41,14 +41,12 @@ const checkUrl = async (url) => {
 const getBrowserData = (req) => {
   const browserDetails = userAgent.parse(req.get("User-Agent"));
   const browser = browserDetails.full.split("/");
-
   return {
     browserName: browser[0],
     browserVersion: browser[1],
     osVersion: browserDetails.os,
   };
 };
-
 module.exports = function (app) {
   app.post("/users", async (request, response) => {
     const img = await checkUrl(request.body.imageUrl).then((result) => {
@@ -57,7 +55,7 @@ module.exports = function (app) {
     if (hasWhiteSpaces(request.body.username)) {
       return response.status(400).json({ errorMessage: "has white spaces" });
     }
-    if (isLenghtOk(request.body.username)) {
+    if (isLengthOk(request.body.username)) {
       return response.status(400).json({
         errorMessage: "username length must be between 3 and 16 characters",
       });
@@ -76,9 +74,9 @@ module.exports = function (app) {
 
     const newUser = {
       uuid: crypto.randomUUID(),
-      username: request.body.username,
+      username: request.body.username.trim(),
       dataTime: new Date().toString(),
-      image: request.body.imageUrl,
+      image: request.body.imageUrl.trim(),
       browser: getBrowserData(request),
     };
     users.push(newUser);
